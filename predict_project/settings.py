43549b3b4c655b9efcbe0f729a04a1adbb1c17a6
codewithgabriel <olatoyeolaide@gmail.com>
2023-05-13 07:28:48 +0200

    first deploy

--- predict_project/settings.py
+++ predict_project/settings.py
@@ -91,9 +91,16 @@ WSGI_APPLICATION = 'predict_project.wsgi.application'
 #     }
 # }
 
+DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': dj_database_url.config(),
 }
+
+
+
+#DATABASES = {
+#    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
