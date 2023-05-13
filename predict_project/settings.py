08f4074256ab36c89aa00faad24f41ff79ab5902
codewithgabriel <olatoyeolaide@gmail.com>
2023-05-13 08:00:51 +0200

    first deploy

--- predict_project/settings.py
+++ predict_project/settings.py
@@ -28,7 +28,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-9_*#^#lmm$lfs9bsb3q9l%66loyk4%$q(t64035c-y%7d38zqb')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
